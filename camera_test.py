0fd1e5753c7a3489d395906d54ffc1ab199cfbb6
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-10-07 18:04:47 -0400

    add camera test

--- camera_test.py
+++ camera_test.py
@@ -3,11 +3,11 @@ from libcamera import controls
 
 def main():
     picam2 = Picamera2()
-    picam2.start(show_preview=False)
+    #picam2.start(show_preview=False)
     picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous,
                          "AfSpeed": controls.AfSpeedEnum.Fast})
 
-    picam2.start_and_capture_files("./fastfocus-test{:d}.jpg", num_files=1, delay=0)
+    picam2.capture_file("./camera-test.jpg")
 
 if __name__ == "__main__":
     main()
