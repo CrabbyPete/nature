c56b7870d4bbc9fc8c067b6b1fa5375c8af60b79
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-10-08 19:43:27 -0400

    run the camera in the background

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import pathlib
 import logging
 import RPi.GPIO as GPIO
 
-from subprocess import call, Popen
+from subprocess import call
+from threading import Thread
 
 # Logging all of the camera's activity to the "naturebytes_camera_log" file. If you want to watch what your camera
 # is doing step by step you can open a Terminal window and type "cd /Naturebytes/Scripts" and then type
@@ -31,6 +32,56 @@ def what_os():
         os_release = dict(reader)
     return os_release
 
+
+def take_photo(command, save_to, use_overlay):
+    """
+    Take a photo and if necessary add overlay
+    :param command: str: command string to send
+    :param use_overlay: bool: use an overlay or not
+    :return: None
+    """
+    # Recording that a PIR trigger was detected and logging the battery level at this time
+    logging.info('PIR trigger detected')
+    # logging.info('Battery level is %(get_batt_level)s', { 'get_batt_level': batt_state })
+
+    # Assigning a variable so we can create a photo JPG file that contains the date and time as its name
+    now = arrow.now().format('YYYY-MM-DD_HH:mm:ss')
+    photo = now +'.jpg'
+
+    # Using the raspistill library to take a photo and show that a photo has been taken in a small preview box on the desktop
+    cmd = f'{command} --output ./{photo}'
+    logging.info(f"cmd:{cmd}")
+
+    # Log that we have just taking a photo"
+    logging.info(f'About to take a photo {photo}')
+    call([cmd], shell=True)
+
+
+    # Log that a photo was taken successfully and state the file name so we know which one"
+    logging.info('Photo taken successfully %(show_photo_name)s', {'show_photo_name': photo})
+    photo_location =  save_to + photo
+
+    if use_overlay:
+        # Log that we are about to attempt to write the overlay text"
+        logging.info('About to write the overlay text')
+
+        overlay = "/usr/bin/convert " + photo_location + " "
+
+        # Use ImageMagick to write text and meta data onto the photo.
+        # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + '" '" + get_time '" -gravity southeast -annotate +6+6 'Camera 1 " "'" + photo_location
+        overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + now + "' -gravity southeast -annotate +6+6 'Camera 1' " + photo_location
+
+        # Log that we the text was added successfully"
+        logging.info('Added the overlay text successfully')
+        call([overlay], shell=True)
+
+        # Add a small Naturebytes logo to the top left of the photo. Note - you could change this to your own logo if you wanted.
+        logging.info('Adding the Naturebytes logo')
+        overlay = '/usr/bin/convert ' + photo_location + ' ./naturebytes_logo_80.png -geometry +1+1 -composite ' + photo_location
+        call([overlay], shell=True)
+
+        # Log that the logo was added successfully"
+        logging.info('Logo added successfully')
 
 def main(use_overlay=False, save_to='./'):
     prev_state = curr_state = 0
@@ -57,60 +108,12 @@ def main(use_overlay=False, save_to='./'):
             # print "Battery level detected via pin %s is %s" % (lowbattPin, newBattState)
 
             if curr_state:  # Our state has changed, so that must be a trigger from the PIR
-                # batt_state = newBattState
-                # Checking the current status of the battery
-
-                # Recording that a PIR trigger was detected and logging the battery level at this time
-                logging.info('PIR trigger detected')
-                # logging.info('Battery level is %(get_batt_level)s', { 'get_batt_level': batt_state })
-
-                # Assigning a variable so we can create a photo JPG file that contains the date and time as its name
-                now = arrow.now().format('YYYY-MM-DD_HH:mm:ss')
-                photo = now +'.jpg'
-
-                # Using the raspistill library to take a photo and show that a photo has been taken in a small preview box on the desktop
-                cmd = f'{cam_command} --output ./{photo}'
-                logging.info(f"cmd:{cmd}")
-
-                # Log that we have just taking a photo"
-                logging.info(f'About to take a photo {photo}')
-                #call ([cmd], shell=True)
-                task = Popen(cmd.split(),
-                             shell=False,
-                             stdin=None,
-                             stdout=None,
-                             stderr=None,
-                             close_fds=True)
-
-                # Log that a photo was taken successfully and state the file name so we know which one"
-                logging.info('Photo taken successfully %(show_photo_name)s', {'show_photo_name': photo})
-                photo_location =  save_to + photo
-
-                if use_overlay:
-                    # Log that we are about to attempt to write the overlay text"
-                    logging.info('About to write the overlay text')
-
-                    overlay = "/usr/bin/convert " + photo_location + " "
-
-                    # Use ImageMagick to write text and meta data onto the photo.
-                    # overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + get_date + '" '" + get_time '" -gravity southeast -annotate +6+6 'Camera 1 " "'" + photo_location
-                    overlay += " -gravity north -background black -extent +0+40 +repage -box black -fill white -pointsize 24 -gravity southwest -annotate +6+6 'Naturebytes Wildlife Cam Kit | Date & Time: " + now + "' -gravity southeast -annotate +6+6 'Camera 1' " + photo_location
-
-                    # Log that we the text was added successfully"
-                    logging.info('Added the overlay text successfully')
-                    call([overlay], shell=True)
-
-                    # Add a small Naturebytes logo to the top left of the photo. Note - you could change this to your own logo if you wanted.
-                    logging.info('Adding the Naturebytes logo')
-                    overlay = '/usr/bin/convert ' + photo_location + ' ./naturebytes_logo_80.png -geometry +1+1 -composite ' + photo_location
-                    call([overlay], shell=True)
-
-                    # Log that the logo was added successfully"
-                    logging.info('Logo added successfully')
+                task = Thread(target=take_photo, args=[cam_command, save_to, use_overlay])
+                task.run()
             else:
                 # print "Waiting for a new PIR trigger to continue"
                 logging.info('Waiting for a new PIR trigger to continue')
 
 
 if __name__ == "__main__":
-    main()
+    main(use_overlay=True)
